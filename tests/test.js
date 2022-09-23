9175d3b07951887c532cb156e96411b3d50122b0
Carson Farmer <carson.farmer@gmail.com>
2022-09-23 11:08:51 -0700

    fix: better input checks + tests + maxquerysize
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- tests/test.js
+++ tests/test.js
@@ -2,39 +2,70 @@
 // eslint-disable-next-line no-unused-vars
 import _init, { initSync, __wasm, init } from "@tableland/sqlparser";
 import assert from "assert";
-import { test, before } from "mocha";
+import { test, before, describe } from "mocha";
 
-before(async function () {
-  await init();
-});
+describe("sqlparser", function () {
+  before(async function () {
+    await init();
+  });
 
-test("failing", async function () {
-  try {
-    await globalThis.sqlparser.normalize(
-      "create table blah_5_ (id int, image blah, description text)"
+  test("failing", async function () {
+    try {
+      await globalThis.sqlparser.normalize(
+        "create table blah_5_ (id int, image blah, description text)"
+      );
+      throw new Error("wrong error");
+    } catch (err) {
+      assert.equal(err.message, "syntax error at position 40 near 'blah'");
+    }
+  });
+
+  test("missing args", async function () {
+    try {
+      await globalThis.sqlparser.normalize();
+      throw new Error("wrong error");
+    } catch (err) {
+      assert.equal(err.message, "missing required argument 'statement'");
+    }
+  });
+
+  test("passing", async function () {
+    const [result] = await globalThis.sqlparser.normalize(
+      "select * FrOM fake_table_1 WHere something='nothing';"
     );
-    throw new Error("wrong error");
-  } catch (err) {
-    assert.equal(err.message, "syntax error at position 40 near 'blah'");
-  }
-});
+    assert.equal(
+      result,
+      "select * from fake_table_1 where something = 'nothing'"
+    );
+  });
 
-test("passing", async function () {
-  const [result] = await globalThis.sqlparser.normalize(
-    "select * FrOM fake_table_1 WHere something='nothing';"
-  );
-  assert.equal(
-    result,
-    "select * from fake_table_1 where something = 'nothing'"
-  );
-});
+  test("multi-write", async function () {
+    const result = await globalThis.sqlparser.normalize(
+      "insert into blah_5_ values (1, 'three', 'something');update blah_5_ set description='something';"
+    );
+    assert.deepEqual(result, [
+      "insert into blah_5_ values (1, 'three', 'something')",
+      "update blah_5_ set description = 'something'",
+    ]);
+  });
 
-test("multi-write", async function () {
-  const result = await globalThis.sqlparser.normalize(
-    "insert into blah_5_ values (1, 'three', 'something');update blah_5_ set description='something';"
-  );
-  assert.deepEqual(result, [
-    "insert into blah_5_ values (1, 'three', 'something')",
-    "update blah_5_ set description = 'something'",
-  ]);
+  test("settings", async function () {
+    // Also test destructuring
+    const { maxQuerySize } = globalThis.sqlparser;
+    assert.equal(maxQuerySize(), 35000);
+    assert.equal(maxQuerySize(10), 10);
+    assert.equal(maxQuerySize(), 10);
+  });
+
+  test("max query size", async function () {
+    // Also test destructuring
+    const { maxQuerySize, normalize } = globalThis.sqlparser;
+    try {
+      maxQuerySize(10);
+      await normalize("select * FrOM fake_table_1 WHere something='nothing';");
+      throw new Error("wrong error");
+    } catch (err) {
+      assert.equal(err.message, "statement size larger than specified max");
+    }
+  });
 });
