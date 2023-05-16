13fa88c7d39a7be6f33591b95dcf55be76e12f00
Dan Buchholz <dbuchholz30@gmail.com>
2023-05-16 13:44:03 -0700

    test: INSERT with flat sub-select, GROUP BY

--- test/main.test.js
+++ test/main.test.js
@@ -252,9 +252,38 @@ describe("sqlparser", function () {
       );
       deepStrictEqual(tables, ["t1", "t2", "t3", "t4"]);
       deepStrictEqual(statements, [
-        'select `t1`.id,[t2].* from `t1` join [t2] join "t3" join(select * from t4)'
+        'select `t1`.id,[t2].* from `t1` join [t2] join "t3" join(select * from t4)',
       ]);
       strictEqual(type, "read");
+    });
+
+    test("when insert with flat sub-select", async function () {
+      const { type } = await globalThis.sqlparser.normalize(
+        "insert into foo_1337_1 (id) select id from blah_1337_2;"
+      );
+      strictEqual(type, "write");
+    });
+
+    test("when insert with group by", async function () {
+      const { type } = await globalThis.sqlparser.normalize(
+        "insert into foo_1337_1 (id) select id from blah_1337_2 group by id;"
+      );
+      strictEqual(type, "write");
+    });
+
+    test("when insert with group by plus unsupported clause", async function () {
+      await rejects(
+        globalThis.sqlparser.normalize(
+          "insert into foo_1337_1 (id) select id from blah_1337_2 group by id having count(id) > 1;"
+        ),
+        (/** @type {any} */ err) => {
+          strictEqual(
+            err.message,
+            "error parsing statement: 1 error occurred:\n\t* HAVING or GROUP BY clauses are not allowed\n\n"
+          );
+          return true;
+        }
+      );
     });
   });
 
@@ -352,6 +381,7 @@ describe("sqlparser", function () {
         prefix: "t",
       });
     });
+
     test("when provided with a valid table name with multiple chars in prefix", async function () {
       const validatedTable = await globalThis.sqlparser.validateTableName(
         "table_1_2"
